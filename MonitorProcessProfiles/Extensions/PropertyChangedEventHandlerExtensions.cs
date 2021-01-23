ba523ff8902476982a40594475d5476ab0e27184
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2021-01-23 18:28:46 +0100

    Initial commit

---
+++ MonitorProcessProfiles/Extensions/PropertyChangedEventHandlerExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorProcessProfiles.Extensions
+{
+    public static class PropertyChangedEventHandlerExtensions
+    {
+        public static bool ChangeAndNotify<T>(this PropertyChangedEventHandler handler, ref T field, T value, Expression<Func<T>> memberExpression)
+        {
+            //Check expressionn
+            if (memberExpression == null)
+            {
+                throw new ArgumentNullException("memberExpression");
+            }
+
+            //Check expression body
+            var body = memberExpression.Body as MemberExpression;
+
+            if (body == null)
+            {
+                throw new ArgumentException("Lambda must return a property.");
+            }
+
+            //Check if value changed
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                //Set value
+                field = value;
+
+                //Notify
+                var vmExpression = body.Expression as ConstantExpression;
+
+                if (vmExpression != null)
+                {
+                    LambdaExpression lambda = Expression.Lambda(vmExpression);
+                    Delegate vmFunc = lambda.Compile();
+                    object sender = vmFunc.DynamicInvoke();
+
+                    if (handler != null)
+                    {
+                        handler(sender, new PropertyChangedEventArgs(body.Member.Name));
+
+                        return true;
+                    }
+                }
+                
+            }
+
+            return false;
+        }
+    }
+}
---
+++ MonitorProcessProfiles/Models/MainWindowVM.cs
@@ -0,0 +1,55 @@
+﻿using MonitorProcessProfiles.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorProcessProfiles.Models
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private int normalBrightness;
+        public int NormalBrightness
+        {
+            get { return normalBrightness; }
+            set { PropertyChanged.ChangeAndNotify(ref normalBrightness, value, () => NormalBrightness); }
+        }
+
+        private int normalContrast;
+        public int NormalContrast
+        {
+            get { return normalContrast; }
+            set { PropertyChanged.ChangeAndNotify(ref normalContrast, value, () => NormalContrast); }
+        }
+
+        private int gamingBrightness;
+        public int GamingBrightness
+        {
+            get { return gamingBrightness; }
+            set { PropertyChanged.ChangeAndNotify(ref gamingBrightness, value, () => GamingBrightness); }
+        }
+
+        private int gamingContrast;
+        public int GamingContrast
+        {
+            get { return gamingContrast; }
+            set { PropertyChanged.ChangeAndNotify(ref gamingContrast, value, () => GamingContrast); }
+        }
+
+        private List<string> processes;
+        public List<string> Processes
+        {
+            get { return processes; }
+            set { PropertyChanged.ChangeAndNotify(ref processes, value, () => Processes); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;        
+
+        public MainWindowVM()
+        {
+            Processes = new List<string>();
+        }
+    }
+}
